4cc8b76473943a88feb914c2c83b723bf400afdd
jibingrg <jibingrg12@gmail.com>
2020-12-24 17:15:10 +0530

    Memory allocated for driver data
    Some memory allocated for driver data in probe function. The V4L2 Skeleton Driver will read data from this memory region.

--- v4l2_skeleton.c
+++ v4l2_skeleton.c
@@ -4,6 +4,7 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/device.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL v2");
 
@@ -12,15 +13,36 @@ static struct platform_device dummy_device = {
 	.id = PLATFORM_DEVID_NONE,
 };
 
+struct driver_data {
+	int data;
+};
+
 static int dummy_probe(struct platform_device *pdev)
 {
-	dev_info(&pdev->dev, "%s\n", __func__);
+	struct driver_data *drvdata;
+
+	dev_info(&pdev->dev, "%s", __func__);
+	drvdata = kzalloc(sizeof(struct driver_data), GFP_KERNEL);
+	if (!drvdata) {
+		dev_err(&pdev->dev, "%s: Memory allocation failed..", __func__);
+		return -ENOMEM;
+	}
+	dev_info(&pdev->dev, "%s: Memory allocated for driver data..", __func__);
+	platform_set_drvdata(pdev, drvdata);
 	return 0;
 }
 
 static int dummy_remove(struct platform_device *pdev)
 {
-	dev_info(&pdev->dev, "%s\n", __func__);
+	struct driver_data *drvdata;
+
+	dev_info(&pdev->dev, "%s", __func__);
+	drvdata = platform_get_drvdata(pdev);
+	if (drvdata) {
+		platform_set_drvdata(pdev, NULL);
+		dev_info(&pdev->dev, "%s: Free allocated memory..", __func__);
+		kfree(drvdata);
+	}
 	return 0;
 }
 
@@ -37,15 +59,19 @@ static int __init skeleton_init(void)
 {
 	int ret;
 
-	pr_info("%s\n", __func__);
+	pr_info("%s", __func__);
 	platform_device_register(&dummy_device);
 	ret = platform_driver_probe(&dummy_driver, dummy_probe);
+	if (ret)
+		pr_err("%s: dummy device is not registered..", __func__);
+	else
+		pr_info("%s: dummy device registered successfully..", __func__);
 	return ret;
 }
 
 static void __exit skeleton_exit(void)
 {
-	pr_info("%s\n", __func__);
+	pr_info("%s", __func__);
 	platform_driver_unregister(&dummy_driver);
 }
 
