95f9ea1a62561d4c9bc78f2ca8b7332954d53cb6
jibingrg <jibingrg12@gmail.com>
2020-12-24 14:15:12 +0530

    A dummy device created
    A dummy platform device is created. This device will act as video device for V4L2 Skeleton driver. The function, platform_driver_probe() is called from init function. So, the probe function can be invoked without having real hardware.

--- v4l2_skeleton.c
+++ v4l2_skeleton.c
@@ -2,18 +2,51 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/device.h>
 
 MODULE_LICENSE("GPL v2");
 
+static struct platform_device dummy_device = {
+	.name = "v4l2_dummy_device",
+	.id = PLATFORM_DEVID_NONE,
+};
+
+static int dummy_probe(struct platform_device *pdev)
+{
+	dev_info(&pdev->dev, "%s\n", __func__);
+	return 0;
+}
+
+static int dummy_remove(struct platform_device *pdev)
+{
+	dev_info(&pdev->dev, "%s\n", __func__);
+	return 0;
+}
+
+static struct platform_driver dummy_driver = {
+	.probe = dummy_probe,
+	.remove = dummy_remove,
+	.driver = {
+		.name = "v4l2_dummy_device",
+		.owner = THIS_MODULE,
+	},
+};
+
 static int __init skeleton_init(void)
 {
+	int ret;
+
 	pr_info("%s\n", __func__);
-	return 0;
+	platform_device_register(&dummy_device);
+	ret = platform_driver_probe(&dummy_driver, dummy_probe);
+	return ret;
 }
 
 static void __exit skeleton_exit(void)
 {
 	pr_info("%s\n", __func__);
+	platform_driver_unregister(&dummy_driver);
 }
 
 module_init(skeleton_init);
