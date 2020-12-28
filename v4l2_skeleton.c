1a7377d8b5ee9e276b6b46c46ed8fc2be937e924
jibingrg <jibingrg12@gmail.com>
2020-12-28 18:35:12 +0530

    Fix bug
    Bug: The module was not able to reinsert.
    Reason: The device was not unregistered in exit function.
    Solution: The fuction, platform_device_unregister is called in exit function.

--- v4l2_skeleton.c
+++ v4l2_skeleton.c
@@ -8,9 +8,17 @@
 
 MODULE_LICENSE("GPL v2");
 
+static void dummy_device_release(struct device *dev)
+{
+	dev_info(dev, "%s", __func__);
+}
+
 static struct platform_device dummy_device = {
-	.name = "v4l2_dummy_device",
+	.name = KBUILD_MODNAME,
 	.id = PLATFORM_DEVID_NONE,
+	.dev = {
+		.release = dummy_device_release,
+	},
 };
 
 struct driver_data {
@@ -50,29 +58,33 @@ static struct platform_driver dummy_driver = {
 	.probe = dummy_probe,
 	.remove = dummy_remove,
 	.driver = {
-		.name = "v4l2_dummy_device",
+		.name = KBUILD_MODNAME,
 		.owner = THIS_MODULE,
 	},
 };
 
-static int __init skeleton_init(void)
+static int skeleton_init(void)
 {
 	int ret;
 
 	pr_info("%s", __func__);
 	platform_device_register(&dummy_device);
+	pr_info("%s: platform device, %s is registered..", __func__, dummy_device.name);
 	ret = platform_driver_probe(&dummy_driver, dummy_probe);
 	if (ret)
-		pr_err("%s: dummy device is not registered..", __func__);
+		pr_err("%s: platform driver, %s is not registered..", __func__, dummy_driver.driver.name);
 	else
-		pr_info("%s: dummy device registered successfully..", __func__);
+		pr_info("%s: platform driver,%s registered successfully..", __func__, dummy_driver.driver.name);
 	return ret;
 }
 
-static void __exit skeleton_exit(void)
+static void skeleton_exit(void)
 {
 	pr_info("%s", __func__);
+	pr_info("%s: platform driver, %s is going to be unregistered..", __func__, dummy_driver.driver.name);
 	platform_driver_unregister(&dummy_driver);
+	pr_info("%s: platform device, %s is going to be unregistered..", __func__, dummy_device.name);
+	platform_device_unregister(&dummy_device);
 }
 
 module_init(skeleton_init);
